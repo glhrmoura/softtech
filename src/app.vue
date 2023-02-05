db26d764586b247de0bdcb2b14739eac5c742557
Guilherme Moura <mouraggui@gmail.com>
2023-02-05 19:27:46 -0300

    feat: app main component

---
+++ src/app.vue
@@ -0,0 +1,34 @@
+<template>
+  <Platform>
+    <template slot="top">
+      <Header />
+    </template>
+    <template slot="inner">
+      <router-view />
+    </template>
+    <template slot="bottom">
+      <MobileNav />
+    </template>
+    <template slot="footer">
+      <Footer />
+    </template>
+  </Platform>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+import Platform from '@/components/Platform.vue';
+import MobileNav from '@/components/MobileNav.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Header,
+    Footer,
+    Platform,
+    MobileNav,
+  },
+};
+</script>
